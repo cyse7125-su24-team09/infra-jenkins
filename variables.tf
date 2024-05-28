a677c2a00e6e0001a2aba8815ea760bc1450ec3c
nitesh <more.ni@northeastern.edu>
2024-05-28 13:01:04 -0400

    AWS VPC Setup

--- variables.tf
+++ variables.tf
@@ -7,3 +7,89 @@ variable "region" {
   type        = string
   description = "AWS Region Name"
 }
+
+
+variable "vpc_tag" {
+  type = string
+
+}
+
+variable "subnet_tag" {
+  type = string
+
+}
+
+variable "vpc_cidr" {
+  type = string
+
+
+}
+
+variable "subnet_cidr" {
+  type = string
+
+
+}
+
+variable "ig_tag" {
+  type = string
+
+}
+
+variable "route_cidr_range" {
+  type = string
+
+}
+variable "route_table_tag" {
+  type = string
+
+}
+
+variable "security_group_name" {
+  type = string
+
+}
+
+variable "security_group_tag" {
+  type = string
+
+}
+
+variable "allow_http_protocol" {
+  type = string
+
+}
+
+variable "allow_http_cidr" {
+  type = string
+
+}
+
+variable "allow_http_port" {
+  type = string
+
+}
+
+variable "allow_https_protocol" {
+  type = string
+
+}
+
+variable "allow_https_cidr" {
+  type = string
+
+}
+
+variable "allow_https_port" {
+  type = string
+
+}
+
+variable "egress_cidr" {
+  type = string
+
+}
+variable "egress_protocol" {
+  type = string
+
+}
